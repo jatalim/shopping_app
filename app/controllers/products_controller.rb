e135c45dede8e0f8a929084d50c4a94996658a57
jatalim <jacinta.lim@insead.edu>
2017-10-04 01:28:47 +0800

    Other

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,34 @@
 class ProductsController < ApplicationController
+before_action :authenticate_admin!,  only: [:new, :edit, :create, :update]
 
     def index
       @products = Product.all
-      @cartedproduct = CartedProduct.new
+    end 
+
+    def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+    end 
+
+    def edit
+      @product = Product.find(params[:id])
+    end 
+
+    def update 
+      product_params = params[:product].permit(:name,:description,:price,:quantity,:image)
+      @product = Product.find(params[:id])
+      @product.update(product_params)
+      redirect_to products_path
     end 
 
     def new
       @product = Product.new
     end
+
+    def show 
+      @product = Product.find(params[:id])
+    end 
 
     def create
       product_params = params[:product].permit(:name,:description,:price,:quantity,:image)
@@ -20,10 +41,6 @@ class ProductsController < ApplicationController
           flash["info"] = "There was an error saving your product"
           render :new
       end
-    end
-
-    def destroy
-
     end
 
 end 
