411b7c54975cf843614647b4d48ec8ce312a711f
jatalim <jacinta.lim@insead.edu>
2017-10-03 16:21:31 +0800

    Admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :public_users
-  devise_for :admins
+
+devise_for :public_users, path: 'public_users', controllers: { sessions: "public_users/sessions", registrations: "public_users/registrations" }
+devise_for :admins, path: 'admins', controllers: { sessions: "admins/sessions", registrations: "admins/registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 root to: "products#index"
+
+get 'admins/dashboard' => 'admins#dashboard'
 
 end
