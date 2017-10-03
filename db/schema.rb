4b244f3ab58f7ef8ecb3f2fe5d197b33afbc30d6
sha <urabummer4@hotmail.com>
2017-10-03 18:02:56 +0800

    css for user update profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20171003072006) do
 
   # These are extensions that must be enabled in order to support this database
@@ -79,6 +78,7 @@ ActiveRecord::Schema.define(version: 20171003072006) do
     t.string "uid"
     t.string "name"
     t.text "image"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_public_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_public_users_on_reset_password_token", unique: true
   end
