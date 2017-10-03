0148b8f60241d939fae064277f6b3def3c184d38
jatalim <jacinta.lim@insead.edu>
2017-10-04 01:09:43 +0800

    Edit and Delete

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,30 @@
 class ProductsController < ApplicationController
+before_action :authenticate_admin!,  only: [:new, :edit, :create, :update]
 
     def index
       @products = Product.all
     end 
 
+    def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    redirect_to products_path
+    end 
+
+    def edit
+      @product = Product.find(params[:id])
+    end 
+
+    def update 
+      product_params = params[:product].permit(:name,:description,:price,:quantity,:image)
+      @product = Product.find(params[:id])
+      @product.update(product_params)
+      redirect_to products_path
+    end 
+
     def new
       @product = Product.new
     end
-
 
     def show 
       @product = Product.find(params[:id])
