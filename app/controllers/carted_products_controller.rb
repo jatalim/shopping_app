c562076be070c4ac29e504e3020003fc29591fb7
jatalim <jacinta.lim@insead.edu>
2017-10-04 13:24:51 +0800

    Other

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,27 +1,27 @@
-class CartedProductsController < ApplicationController
+# class CartedProductsController < ApplicationController
 
-  def index
-    @cartedproducts = CartedProduct.where(public_user_id: current_public_user.id).order("created_at DESC")
-  end
+#   def index
+#     @cartedproducts = CartedProduct.where(public_user_id: current_public_user.id).order("created_at DESC")
+#   end
 
-  def create
+#   def create
     
-    cartedproduct_params = params[:carted_product].permit(:product_quantity,:product_id,:public_user_id)
+#     cartedproduct_params = params[:carted_product].permit(:product_quantity,:product_id,:public_user_id)
 
-    @cartProd = CartedProduct.new(cartedproduct_params)
+#     @cartProd = CartedProduct.new(cartedproduct_params)
 
-    if @cartProd.save
-      flash["info"] = "Product added to cart"
-    else
-      flash["info"] = "There was an error adding product to cart"
-    end
+#     if @cartProd.save
+#       flash["info"] = "Product added to cart"
+#     else
+#       flash["info"] = "There was an error adding product to cart"
+#     end
 
-    redirect_to root_path
+#     redirect_to root_path
 
-  end
+#   end
 
-  def destroy
+#   def destroy
 
-  end
+#   end
 
-end
+# end
