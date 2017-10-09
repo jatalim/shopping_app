cc459974a28c40b858aee18a8e00d77f6cc3069c
Kedanco <kelvinchowjunjie120@hotmail.com>
2017-10-09 17:55:44 +0800

    Tag creation, assignment, view added into admin dashboard

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,7 +1,13 @@
 class AdminsController < ApplicationController
 
     def dashboard
-      @productTag = ProductTagging.new
+      @tag = Tag.new
+      @productTagging = ProductTagging.new
+
+      taglist = ProductTagging.select(:tag_id)
+      @floatingTags = Tag.where.not(id: taglist)
+
+      @products = Product.all
     end 
 
 
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,8 +1,8 @@
 class Tag < ApplicationRecord
 
-  has_many :product_taggings
+  has_many :product_taggings, dependent: :destroy
   has_many :products, :through => :product_taggings
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 
 end
